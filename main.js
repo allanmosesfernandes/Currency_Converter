85642cb9094851ddce6c4e2fcddbc42bbe11c209
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-01 14:01:18 +0100

    caching base rates

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fromOptions = document.querySelector('[name="from_currency"]');
 const toOptions = document.querySelector('[name="to_currency"]');
 
+const baseRates = {}
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -46,24 +47,42 @@ const generateOptions = (options) => {
 
 //Fetch Rates
 async function fetchRates(base = 'USD') {
-const myHeaders = new Headers();
-myHeaders.append("apikey", "EyBfpidfvZOVQORvq2KGxLKZ2JScMtmF")
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", "EyBfpidfvZOVQORvq2KGxLKZ2JScMtmF")
 
-const requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-};
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
 
-const response = await fetch(`https://api.apilayer.com/exchangerates_data/latest?base=${base}`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-  return response;
+    const response = await fetch(`https://api.apilayer.com/exchangerates_data/latest?base=${base}`, requestOptions)
+      .then(response => response.json())
+      .then(result => result)
+      .catch(error => console.log('error', error));
+      
+    return response;
 }
+
+
 const optionsHTML = generateOptions(currencies);
 fetchRates();
 fromOptions.innerHTML = optionsHTML;
 toOptions.innerHTML = optionsHTML;
 
+async function getRates(amount, from, to) {
+    if(!baseRates[from]) {
+        console.log('leke ana padega');
+        const rates = await fetchRates(from);
+
+        //store them for next time
+        baseRates[from] = rates;
+    }
+
+    //converted amount
+    const rate = baseRates[from].rates[to];
+    const convertedAmount = rate * amount;
+    console.log(`${amount} from ${from} to ${to} is : ${convertedAmount}`)
+}
+
+console.log(getRates(100, 'INR', 'GBP'))
